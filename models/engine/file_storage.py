d93d930fa01fbcbe22724a553774f2e7c89e8bee
FadwaLacham <fadwa.lacham@gmail.com>
2024-02-11 06:06:48 -0800

    fixing problems

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ and from json
 """
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -39,6 +45,12 @@ class FileStorage:
         """
         class_dict = {
             "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
         obj = FileStorage.__objects
         try:
